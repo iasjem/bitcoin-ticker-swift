4236c2e51cf63f8970260dfb51ee439ff742313e
jem.sai <jem.sai@whitecloak.com>
2018-07-30 19:12:34 +0800

    Projects - added new files

---
+++ bitcoin-ticker/Utilities/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  bitcoin-ticker
+//
+//  Created by Jemimah Beryl M. Sai on 30/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import Foundation
