b3f584bd45713abeb6c93f6fbca8a5c7c76c8eeb
jem.sai <jem.sai@whitecloak.com>
2018-08-02 17:26:08 +0800

    CurrencyCode - added new file

---
+++ bitcoin-ticker/Enums/CurrencyCode.swift
@@ -0,0 +1,35 @@
+//
+//  Constants.swift
+//  bitcoin-ticker
+//
+//  Created by Jemimah Beryl M. Sai on 30/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import Foundation
+
+enum CurrencyCode: String {
+    case USD
+    case JPY
+    case CNY
+    case SGD
+    case HKD
+    case CAD
+    case NZD
+    case AUD
+    case CLP
+    case GBP
+    case DKK
+    case SEK
+    case ISK
+    case CHF
+    case BRL
+    case EUR
+    case RUB
+    case PLN
+    case THB
+    case KRW
+    case TWD
+    
+    static let values = [USD, JPY, CNY, SGD, HKD, CAD, NZD, AUD, CLP, GBP, DKK, SEK, ISK, CHF, BRL, EUR, RUB, PLN, THB, KRW, TWD]
+}
