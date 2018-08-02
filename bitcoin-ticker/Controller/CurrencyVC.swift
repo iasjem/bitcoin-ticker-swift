8d7ea6ee2e19eb28ba8a766741c67e2475178b6d
jem.sai <jem.sai@whitecloak.com>
2018-08-02 17:27:30 +0800

    CurrencyVC - setup tableview for data to be displayed

--- bitcoin-ticker/Controller/CurrencyVC.swift
+++ bitcoin-ticker/Controller/CurrencyVC.swift
@@ -12,9 +12,59 @@ class CurrencyVC: UIViewController {
     
     @IBOutlet weak var loadingView: UIStackView!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var service = CurrencyService.sharedInstance
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        viewExchangeRate()
+    }
+    
+    
+    func viewExchangeRate() {
+        service.retrieveData { (success) in
+            if success {
+            self.loadedData()
+            } else {
+            self.loadingData()
+            }
+            self.tableView.reloadData()
+        }
+    }
+    
+    func loadingData() {
+        service.clearData()
+        self.tableView.isHidden = true
+        self.loadingView.isHidden = false
+        self.spinner.startAnimating()
+    }
+    
+    func loadedData(){
+        self.tableView.isHidden = false
+        self.loadingView.isHidden = true
+        self.spinner.stopAnimating()
+    }
+}
+
+extension CurrencyVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return service.currency.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CurrencyCell", for: indexPath) as? CurrencyCell else {
+            return UITableViewCell()
+        }
+        let currency = service.currency[indexPath.row]
+        cell.configureCell(code: currency.currencyCode, symbol: currency.currencySymbol, price: currency.marketPrice)
+        return cell
     }
 }
 
