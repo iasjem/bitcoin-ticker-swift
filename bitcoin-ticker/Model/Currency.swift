d50ab276af61e442a8e5f97c8cde75c6c0131f5c
jem.sai <jem.sai@whitecloak.com>
2018-08-02 17:25:30 +0800

    Currency - added new file

---
+++ bitcoin-ticker/Model/Currency.swift
@@ -0,0 +1,21 @@
+//
+//  ExchangeRate.swift
+//  bitcoin-ticker
+//
+//  Created by Jemimah Beryl M. Sai on 31/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import Foundation
+
+struct Currency {
+    var currencyCode: String
+    var marketPrice: Double
+    var currencySymbol: String
+    
+    init (code: String, marketPrice: Double, symbol: String) {
+        self.currencyCode = code
+        self.marketPrice = marketPrice
+        self.currencySymbol = symbol
+    }
+}
